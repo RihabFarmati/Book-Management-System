deaf88e4f9ff16e17a3731c5d59e3157d5652947
rihabFarmati <rihabfarmati19@gmail.com>
2024-03-16 12:49:07 +0000

    Optimize the imports

--- src/main/java/com/example/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/example/filter/CustomAuthenticationFilter.java
@@ -13,11 +13,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
