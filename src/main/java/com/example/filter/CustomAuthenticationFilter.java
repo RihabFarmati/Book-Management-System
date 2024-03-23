64dd582072fe48813b08d66296be575e4192a529
rihabFarmati <rihabfarmati19@gmail.com>
2024-03-23 10:13:27 +0000

    update securityConfig

--- src/main/java/com/example/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/example/filter/CustomAuthenticationFilter.java
@@ -56,7 +56,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
         String refreshToken = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 600 * 1000))
                 .withIssuer(request.getRequestURL().toString())
                 .sign(algorithm);
 
