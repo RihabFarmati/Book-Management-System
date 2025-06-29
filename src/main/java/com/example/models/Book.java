1d280663c94cec639328b97f7f0a6d8a3689c097
rihabFarmati <rihabfarmati19@gmail.com>
2025-06-29 18:42:36 +0100

    Add createdAt Timestamp

--- src/main/java/com/example/models/Book.java
+++ src/main/java/com/example/models/Book.java
@@ -4,11 +4,13 @@ package com.example.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -27,4 +29,8 @@ public class Book {
     private Double price;
 
     private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
 }
